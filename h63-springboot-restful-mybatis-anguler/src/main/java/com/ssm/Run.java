b41cc5854f6cbaeefd8f5813c945e62bb3b5a4bf
pidchen <254476367@qq.com>
2021-08-19 11:33:49 +0800

    initial commit

--- h63-springboot-restful-mybatis-anguler/src/main/java/com/ssm/Run.java
+++ h63-springboot-restful-mybatis-anguler/src/main/java/com/ssm/Run.java
@@ -11,9 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Run {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Run.class, args);
-//		Runtime.getRuntime().exec(
-//				"cmd   /c   start chrome.exe  http://localhost:8080/ssm/home.html");
-		Runtime.getRuntime().exec(new String[]{"/usr/bin/open", "-a", "/Applications/Microsoft Edge.app", "http://localhost:8080" + "/ssm/home.html"});
+		Runtime.getRuntime().exec(
+				"cmd   /c   start chrome.exe  http://localhost:8080/ssm/home.html");
 	}
 
 }
--- h63-springboot-restful-mybatis-anguler/target/classes/import.sql
+++
@@ -1,9 +0,0 @@
-Create TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(50) DEFAULT NULL,
-  `userAge` int(11) DEFAULT NULL,
-  `userAddress` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
-
-Insert INTO `user` VALUES ('1', 'tom凌', '100', '深圳,广东');
